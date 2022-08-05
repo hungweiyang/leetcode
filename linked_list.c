2cdb70c9ba7222b9d9aac0677f352dcc2fdd843b
hungweiyang <38694602+hungweiyang@users.noreply.github.com>
2022-08-05 06:55:40 +0000

    Add Reverse()

--- linked_list.c
+++ linked_list.c
@@ -1,59 +1,55 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-struct ListNode
-{
-	int val;
-	struct ListNode *next;
+struct ListNode {
+  int val;
+  struct ListNode *next;
 };
 
-void InsertNode(struct ListNode **phead, int val)
-{
-	struct ListNode **it = phead;
-	while (*it)
-		it = &((*it)->next);
+typedef struct ListNode Node;
 
-	*it = malloc(sizeof(struct ListNode));
-	(*it)->val = val;
-	(*it)->next = NULL;
+void InsertNode(struct ListNode **phead, int val) {
+  struct ListNode **it = phead;
+  while (*it)
+    it = &((*it)->next);
 
+  *it = malloc(sizeof(struct ListNode));
+  (*it)->val = val;
+  (*it)->next = NULL;
 }
 
-void Traverse(struct ListNode *head)
-{
-	struct ListNode *it = head;
-	while (it)
-	{
-		printf("%d\n", it->val);
-		it = it->next;
-	}
-	return;
+void Traverse(struct ListNode *head) {
+  struct ListNode *it = head;
+  while (it) {
+    printf("%d\n", it->val);
+    it = it->next;
+  }
+  return;
 }
 
-struct ListNode *Reverse(struct ListNode *head)
-{
-    struct ListNode *dummy = malloc(sizeof(struct ListNode));
-    dummy->next = head;
+struct ListNode *Reverse(Node *it) {
+  Node *prev = NULL, *next;
 
-    struct ListNode *curr = head;
-    
-    while (curr)
-    {
-        curr->next;
-    }
+  while (it) {
+    next = it->next;
+    it->next = prev;
+    prev = it;
+    it = next;
+  }
+
+  return prev;
 }
 
-int main(int argc, char *argv[])
-{
-	struct ListNode *head = NULL;
+int main(int argc, char *argv[]) {
+  struct ListNode *head = NULL;
 
-	InsertNode(&head, 1);
-	InsertNode(&head, 2);
-	InsertNode(&head, 3);
-	InsertNode(&head, 4);
-	InsertNode(&head, 5);
-	Traverse(head);
+  InsertNode(&head, 1);
+  InsertNode(&head, 2);
+  InsertNode(&head, 3);
+  InsertNode(&head, 4);
+  InsertNode(&head, 5);
+  Traverse(head);
 
-	head = Reverse(head);
-	Traverse(head);
+  head = Reverse(head);
+  Traverse(head);
 }
