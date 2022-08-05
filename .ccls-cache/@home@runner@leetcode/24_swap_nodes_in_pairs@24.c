443ec83e15519d98552a1c50a8f62e6bc9951c1f
hungweiyang <38694602+hungweiyang@users.noreply.github.com>
2022-08-05 07:27:03 +0000

    Update 24.c

--- .ccls-cache/@home@runner@leetcode/24_swap_nodes_in_pairs@24.c
+++ .ccls-cache/@home@runner@leetcode/24_swap_nodes_in_pairs@24.c
@@ -1,28 +1,63 @@
-/**
- * Definition for singly-linked list.
- * struct ListNode {
- *     int val;
- *     struct ListNode *next;
- * };
- */
 
+#include <stdio.h>
+#include <stdlib.h>
 
-struct ListNode* swapPairs(struct ListNode* head){
-    struct ListNode **p1 = &head;
-    struct ListNode **p2 = malloc(sizeof(struct ListNode*) * 2); 
+struct ListNode {
+  int val;
+  struct ListNode *next;
+};
 
-    for (; *p1;)
-    {
-        p2[0] = *p1;
-        p2[1] = (*p1)->next;
-        if (!p2[1])
-            break;
-        
-        *p1 = p2[1];
-        p2[0]->next = p2[1]->next;
-        p2[1]->next = p2[0];
-        p1 = &p2[0]->next;
-    }
-    
-    return head;
+struct ListNode *swapPairs(struct ListNode *it) {
+  struct ListNode *dummy = malloc(sizeof(struct ListNode));
+  struct ListNode *prev = dummy;
+  struct ListNode *next;
+
+  while (it) {
+    next = it->next;
+
+    if (!next)
+      break;
+
+    it->next = next->next;
+    next->next = it;
+    prev->next = next;
+    prev = it;
+    it = it->next;
+  }
+
+  return dummy->next;
+}
+
+void InsertNode(struct ListNode **phead, int val) {
+  struct ListNode **it = phead;
+  while (*it)
+    it = &((*it)->next);
+
+  *it = malloc(sizeof(struct ListNode));
+  (*it)->val = val;
+  (*it)->next = NULL;
+  return;
+}
+
+void Traverse(struct ListNode *head) {
+  struct ListNode *it = head;
+  while (it) {
+    printf("%d\n", it->val);
+    it = it->next;
+  }
+  return;
+}
+
+int main(int argc, char *argv[]) {
+  struct ListNode *head = NULL;
+  InsertNode(&head, 1);
+  InsertNode(&head, 2);
+  InsertNode(&head, 3);
+  InsertNode(&head, 4);
+  InsertNode(&head, 5);
+  Traverse(head);
+
+  printf("----\n");
+  head = swapPairs(head);
+  Traverse(head);
 }
