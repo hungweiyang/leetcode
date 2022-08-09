b5da0a3a431e88c0418a68475f66c1bb18dbe159
hungweiyang <38694602+hungweiyang@users.noreply.github.com>
2022-08-09 14:01:46 +0000

    Update 05.c

--- .ccls-cache/@home@runner@leetcode/05_longest_palindromic_substring@05.c
+++ .ccls-cache/@home@runner@leetcode/05_longest_palindromic_substring@05.c
@@ -1,40 +1,38 @@
 #include <stdio.h>
 #include <string.h>
 
-#define max(m, n) ((m) > (n)) ? (m) : (n) 
+#define max(m, n) ((m) > (n)) ? (m) : (n)
 
-int checkPalindrome(char * s, int start, int len)
-{
-  if (len == 1) return 1;
-
-  for (int i = start; i < start + len/2; i++) {
-    if (s[i] != s[start + len - i])
-      return 1;
+void checkPalindrome(int l, int r, char *s, int *start, int *maxlen) {
+  int len = strlen(s);
+  while (l >= 0 && r < len && s[l] == s[r]) {
+    --l;
+    ++r;
   }
-  return 0;
+
+  ++l;
+  --r;
+  if (*maxlen < (r - l + 1)) {
+    *maxlen = r - l + 1;
+    *start = l;
+  }
 }
 
-char * longestPalindrome(char * s)
-{
-  int l, r, size = 0, start = 0;
+char *longestPalindrome(char *s) {
   int len = strlen(s);
-  for (int i = 0; i < len-1; i++) {
-    for (int j = i+1; j < len; j++) {
-      if (checkPalindrome(s, i, j-i+1) == 0)
-        if (j-i+1 > size) {
-          size = j-i+1;
-          start = i;
-        }
-    }
+  int maxlen = 0;
+  int start = 0;
+  int tmp1, tmp2;
+  for (int i = 0; i < len; i++) {
+    checkPalindrome(i, i, s, &start, &maxlen);
+    checkPalindrome(i, i + 1, s, &start, &maxlen);
   }
 
-  if (size == 0) size = 1;
-  s[start+size] = 0;
+  s[start + maxlen] = 0;
   return &s[start];
 }
 
-int main()
-{
-  char s[] = "ac";
+int main() {
+  char s[] = "";
   printf("%s\n", longestPalindrome(s));
 }
