d9e1637129ee82d92eb0cf44b5f3c150e43a13e7
blocat <blocat0817@gmail.com>
2022-09-17 00:08:10 +0800

    Add 14.cpp

---
+++ 0014_longest_common_prefix/14.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+#include <vector>
+#include <string>
+using namespace std;
+
+class Solution {
+public:
+    string longestCommonPrefix(vector<string>& strs) {
+        string prefix = strs[0];
+        for (string s : strs) {
+            int i;
+            for (i = 0; i < s.size() && i < prefix.size(); ++i) {
+                if (s[i] != prefix[i])
+                    break;
+            }
+            prefix.erase(i, string::npos);
+        }
+        return prefix;
+    }
+};
+
+int main()
+{
+    Solution s;
+    vector<string> q = {"flower", "flow", "fl"};
+    cout << s.longestCommonPrefix(q) << std::endl;
+    return 0;
+}
